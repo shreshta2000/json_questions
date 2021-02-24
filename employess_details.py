eccdfcad3c3c4438404edb53e22fd8aaf7cf2fba
shreshta2000 <69512364+shreshta2000@users.noreply.github.com>
2021-02-24 11:20:26 +0530

    Update employess_details.py

--- employess_details.py
+++ employess_details.py
@@ -5,29 +5,29 @@ list1 = ["name","designation","age","salary"]
 with open(filename) as details: 
     for line in details:
 
-        # for words in line:
-            # print(words)
-        # key = line.strip().split(None,1)
+        for words in line:
+            print(words)
+        key = line.strip().split(None,1)
         print(line)
-        # print(key)
-        # for i in line: 
+        print(key)
+        for i in line: 
 
-        # print(key)
-        # for i in description:
-            # for j in i:
-                # print([i][j])
-        # i = 0
-        # while i < len(list1):
-        #     print(list1[i])
-            # i = i + 1
-        #     j=0
-        #     while j in (description[i]):
+        print(key)
+        for i in description:
+            for j in i:
+                print([i][j])
+        i = 0
+        while i < len(list1):
+            print(list1[i])
+            i = i + 1
+            j=0
+            while j in (description[i]):
 
-        #     # print(i,line[i])
-        #         print(description[i][j])
-        #         j= j + 1
-            # i = i + 1
-            # description = list( line.strip().split(None, 4))
-        # mydict[list1[line]] = value
-        # print(value)
-# print(mydict)
+            print(i,line[i])
+                print(description[i][j])
+                j= j + 1
+            i = i + 1
+            description = list( line.strip().split(None, 4))
+        mydict[list1[line]] = value
+        print(value)
+print(mydict)
